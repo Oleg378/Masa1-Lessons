89682ff1e0187e7bf18c9cdf29811ee26c0c17d2
Oleg <oleg-dmitriewich@yandex.ru>
2023-06-28 20:45:34 +0300

    Еще одна попытка обновить этого гавно

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {        
         System.out.println("Hello, World!");
-        // String name ="Alex";
-        // System.out.println("My name is " + name);  
-        // hgfhg
+        String name ="Alex";
+        System.out.println("My name is " + name);  
+        ///hgfhg
         //jkhjkh
+        //sdfdg
     }
 }
---
+++ hello_masa.java
@@ -0,0 +1,8 @@
+public class hello_masa {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String name ="Oleg";
+        String my_city = "Saint Petersburg";
+        System.out.println("My name is " + name + " my city " + my_city);  
+}
+}
